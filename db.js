3e25a5181ede872b110246f001582a2251784e88
Synetic1 <syneticslz@gmail.com>
2025-08-07 13:51:34 +0100

    update

--- db.js
+++ db.js
@@ -182,7 +182,7 @@ const WideoakUserSchema = new mongoose.Schema({
   },
   subscriptionStatus: {
     type: String,
-    enum: ['free', 'trialing', 'active', 'canceled', 'past_due', 'incomplete', 'incomplete_expired', 'unpaid', 'free-trial'],
+    enum: ['free', 'trialing', 'active', 'canceled', 'past_due', 'incomplete', 'incomplete_expired', 'unpaid', 'free-trial', 'single-search-active'],
     default: 'free',
     index: true
   },
